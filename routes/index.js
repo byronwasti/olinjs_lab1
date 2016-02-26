99ff5911071380df6f8dd9fd757aa0ea747f15b3
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2016-02-26 16:03:05 -0500

    Started on routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
+var Page = require('../models/pageModel.js');
+
 module.exports = function(){
     return {
         home: function(req, res){
-
+        	Page.find({}, function(err, pages){
+        		if (err){
+        			res.send(err).status(500);
+        			console.error(err);
+        		} else {
+        			//send title of pages to each
+        		}
+        	});
         },
-        getPage: function(req, res){
-
-        },
-        postPage: function(req, res){
-
-        },
-        editPage: function(req, res){
-
-        },
-        deletePage: function(req, res){
-        }
+        
     }
 }
