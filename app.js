1588d5c257f6c7e3725cf5de8e94c6deacde4e87
byronwasti <byron.wasti@gmail.com>
2016-02-26 15:45:42 -0500

    remove un-needed

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
-var session = require('express-session');
 var db = require('./db');
 
 var index = require('./routes/index')();
 
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(session({ secret: 'dfasldkfu1n',
-    resave: false,
-    saveUninitialized: false,
-    cookie:{}
-}));
 
 // Base Routes
 app.get('/', index.home);
