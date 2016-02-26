f3c18a3dfcd7d3c3e1a1c83e13caa4472d127645
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2016-02-26 16:27:40 -0500

    Finished api without testing

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,55 @@ module.exports = function(){
         			res.send(err).status(500);
         			console.error(err);
         		} else {
-        			//send title of pages to each
+        			var titles = pages.map(function(page){
+        				return page.title;
+        			});
+        			console.log(titles);
+        			titles = {titles: titles};
+        			res.json(titles);
         		}
         	});
         },
       	getPage: function(req, res){
-
+      		Page.findById(req.query.id, function(err, page){
+      			if (err){
+        			res.send(err).status(500);
+        			console.error(err);
+        		} else {
+        			res.json(page);
+        		}
+      		});
         },
         postPage: function(req, res){
-
+        	var newPage = new Page({title: req.body.title, content: req.body.content});
+        	newPage.save(function(err, page){
+        		if (err){
+        			res.send(err).status(500);
+        			console.error(err);
+        		} else {
+        			res.json(page);
+        		}
+        	});
         },
         editPage: function(req, res){
-
+        	Page.findByIdAndUpdate(req.query.id, {title:req.body.title, content:req.body.content}, {new: true}, function(err, page){
+        		if (err){
+        			res.send(err).status(500);
+        			console.error(err);
+        		} else {
+        			res.json(page);
+        		}
+        	});
 		},
 		deletePage: function(req, res){
-		}
-    }
+			Page.findByIdAndRemove(req.body.id, function(err, removed){
+			 	if (err){
+			 		res.send(err).status(500);
+			 		console.log("Error: ", err);
+			 	} else {
+			 		res.json({sucess: true});
+			 	}
+			});
+    	}
+	}
 }
