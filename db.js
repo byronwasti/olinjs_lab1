8928f7da39e3854a8a71e882e9052286bea7af08
byronwasti <byron.wasti@gmail.com>
2016-02-26 15:37:35 -0500

    init

---
+++ db.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://olinjs:wiki@ds017688.mlab.com:17688/wikipages');
+var connection = mongoose.connection;
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', function(){
+    console.log('Mongodb Connection Successful');
+});
