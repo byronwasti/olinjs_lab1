c60d19c03ac5ba3e68f4f7eff2080d9ea567a827
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2016-03-01 16:05:24 -0500

    Working now

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,12 +11,16 @@ var WikiBox = React.createClass({
         xhr.open('GET', '/api/titles');
         xhr.onreadystatechange = function(){
             if (xhr.readyState == 4 && xhr.status == 200) {
-                this.setState({pageTitles: JSON.parse(xhr.responseText).titles});
+                var titles = JSON.parse(xhr.responseText).titles;
+                var newTitles = titles.map(function(title){
+                    return {title: title.title, _id: title._id, isClicked: false};
+                });
+                this.setState({pageTitles: newTitles});
             }
         }.bind(this);
         xhr.send();
-
-    loadPageFromServer: function(id){
+    },
+    loadPageFromServer: function(id, newTitles){
         xhr.open('GET', '/api/page?id=' + id);
         xhr.onreadystatechange = function(){
             if (xhr.readyState == 4 && xhr.status == 200) {
@@ -26,12 +30,21 @@ var WikiBox = React.createClass({
             }
         }.bind(this);
         xhr.send();
+        var newTitles = this.state.pageTitles.map(function(title){
+            if (title._id === id){
+                title.isClicked = true;
+            } else {
+                title.isClicked = false;
+            }
+            return title;
+        });
+        this.setState({pageTitles: newTitles});
     },
     render: function(){
         return (
 <div className='wikiBox'>
 <NavBar />
-<PageList titles={this.state.pageTitles} getPage={this.loadPageFromServer}/>
+<PageList titles={this.state.pageTitles} getPage={this.loadPageFromServer} setUpPageList={this.setUpPageList}/>
 <PageBox title={this.state.pageTitle} content={this.state.pageContent}/>
 </div>
                );
--- public/javascripts/pageList.js
+++ public/javascripts/pageList.js
@@ -1,25 +1,8 @@
 var PageList = React.createClass({
-    getInitialState: function(){
-      return {titles: this.props.titles.map(function(title){
-        return {title: title.title, _id: title._id, isClicked: false};  
-      })};
-    },
-    getPage: function(id){
-      this.props.getPage(id);
-      var updatedTitles = this.state.titles.map(function(title){
-        if (id === title._id){
-          title.isClicked = true;
-        } else {
-          title.isClicked = false;
-        }
-        return title;
-      });
-      this.setState({titles: updatedTitles});
-    },
     render: function(){
         return (
         <div className='pageList'>
-        {this.state.titles.map(function(title){
+        {this.props.titles.map(function(title){
             return (
         <PageListElement title={title.title} key={title._id} id={title._id} getPage={this.props.getPage} isPressed={title.isClicked}/>
                    );
